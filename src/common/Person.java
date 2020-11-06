4ceb6846212f13bce99c12d1e104ed26d75037dd
wlsqja1012 <Kosmo_23@DESKTOP-QOHO9LP>
2020-11-06 18:01:31 +0900

    Conflict(충돌)예상됨

--- src/common/Person.java
+++ src/common/Person.java
@@ -1,5 +1,14 @@
 package common;
 
 public class Person {
-
+	String name;
+	int age;
+	String nickName;
+	
+	public Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	
 }
